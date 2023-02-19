3b51e40dcecb9bc5af4a73a299d0c90161ebaafb
Diego Eduardo Wiltler <90121713+Diego-Eduardo-Wiltler@users.noreply.github.com>
2023-02-19 17:11:39 -0300

    Update jogo.py

--- jogo.py
+++ jogo.py
@@ -22,48 +22,54 @@ print("=====================================")
 
 ia = random.randint(1, 5)
 
-if resposta == ia:
-    print('Empate')
-
-if resposta == 1:
+if ia == pedra:
     print('O Adversário escolheu pedra')
-    if resposta == papel or resposta == spoky:
+    if resposta == pedra:
+        print('Empate')
+    elif resposta == papel or resposta == spoky:
         print('Você venceu')
     elif resposta == tesoura or resposta == lagarto:
         print('Você perdeu')
     else:
         print('Resposta inválida')
 
-elif resposta == 2:
-    print('O Adversário escolheu papel')
-    if resposta == tesoura or resposta == lagarto:
+elif ia == papel:
+    if resposta == papel:
+        print('Empate')
+    elif resposta == tesoura or resposta == lagarto:
         print('Você venceu')
     elif resposta == pedra or resposta == spoky:
         print('Você perdeu')
     else:
         print('Resposta inválida')
 
-elif ia == 3:
+elif ia == tesoura:
     print('O Adversário escolheu tesoura')
-    if resposta == pedra or resposta == spoky:
+    if resposta == tesoura:
+        print('Empate')
+    elif resposta == pedra or resposta == spoky:
         print('Você venceu')
     elif resposta == papel or resposta == lagarto:
-        print('Você Perdeu')
+        print('Você perdeu')
     else:
         print('Resposta inválida')
 
-elif ia == 4:
+elif ia == spoky:
+    print('O Adversário escolheu spoky')
+    if resposta == spoky:
+        print('Empate')
+    elif resposta == papel or resposta == lagarto:
+        print('Você venceu')
+    elif resposta == tesoura or resposta == pedra:
+        print('Você perdeu')
+    else:
+        print('Resposta inválida')
+
+elif ia == lagarto:
     print('O Adversário escolheu tesoura')
-    if resposta == papel or resposta == lagarto:
-        print('Você Venceu')
-    elif resposta == tesoura or pedra:
-        print('Você Perdeu')
-    else:
-        print('Resposta inválida')
-
-elif ia == 5:
-    print('O adversário escolheu lagarto')
-    if resposta == pedra or tesoura:
+    if resposta == lagarto:
+        print('Empate')
+    elif resposta == pedra or resposta == tesoura:
         print('Você venceu')
     elif resposta == spoky or resposta == papel:
         print('Você perdeu')
